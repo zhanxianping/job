e59ef657018ac453d3a0d82d7fd7ecd1f692c995
zhanxianping <1447919914@qq.com>
2020-07-22 18:09:35 +0800

    头部下拉框和弹框

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,13 @@
                     <img src="../public/image/index/logo.png" alt="">
                 </li>
                 <div v-for="(item,index) in sideNav.data" :key="item.id">
-                    <el-submenu  :index="String(index)" v-if="item.nodes !== undefined && item.nodes.length >= 1">
+                    <el-submenu :index="String(index)" v-if="item.nodes !== undefined && item.nodes.length >= 1">
                         <template slot="title">
                             <i :class="'icon iconfont '+item.icon"></i>
                             <span v-show="!headerNav.sideNavOn">{{item.name}}</span>
                         </template>
-                        <SideNavList v-if="item.nodes !== undefined && item.nodes.length >= 1" :dataList="item.nodes" :listIndex="index"/>
+                        <SideNavList v-if="item.nodes !== undefined && item.nodes.length >= 1" :dataList="item.nodes"
+                                     :listIndex="index"/>
                     </el-submenu>
                     <el-menu-item v-else :index="String(index)">
                         <i :class="'icon iconfont '+item.icon"></i>
@@ -47,14 +48,86 @@
                     </div>
                     <div id="nav-user">
                         <img src="./assets/1.jpg" alt="">
-                        <span>管理员 <i class="el-icon-caret-bottom"></i></span>
+                        <el-dropdown trigger="click" @command="userDrop">
+                            <span class="el-dropdown-link">
+                            管理员<i class="el-icon-arrow-down el-icon--right"></i>
+                            </span>
+                            <el-dropdown-menu slot="dropdown">
+                                <el-dropdown-item command="1"><i class="icon iconfont itsm-client"></i>个人信息
+                                </el-dropdown-item>
+                                <el-dropdown-item command="2"><i class="icon iconfont itsm-yingyong"></i>应用设置
+                                </el-dropdown-item>
+                                <el-dropdown-item command="3"><i class="icon iconfont itsm-gys"></i>主题皮肤
+                                </el-dropdown-item>
+                                <el-dropdown-item command="4"><i class="icon iconfont itsm-gys"></i>关于系统
+                                </el-dropdown-item>
+                                <el-dropdown-item command="5"><i class="icon iconfont itsm-logout"></i>退出
+                                </el-dropdown-item>
+                            </el-dropdown-menu>
+                        </el-dropdown>
                     </div>
-                    <i class="el-icon-bell nav-news"></i>
+                    <el-badge :value="134" class="item">
+                        <i size="small" class="el-icon-bell nav-news"></i>
+                    </el-badge>
+
                     <div class="nav-language">
-                        <img src="../public/image/index/ChinaLogo.png" alt="">
-                        <span>中文</span>
+                        <img :src="headerNav.language.img" alt="">
+                        <el-dropdown trigger="click" @command="languageDrop">
+                            <span class="el-dropdown-link">{{headerNav.language.text}}</span>
+                            <el-dropdown-menu slot="dropdown">
+                                <el-dropdown-item command="china"><img style="margin-right: 5px;vertical-align: middle;"
+                                                                       src="../public/image/index/ChinaLogo.png" alt="">中文
+                                </el-dropdown-item>
+                                <el-dropdown-item command="english"><img
+                                        style="margin-right: 5px;vertical-align: middle;"
+                                        src="../public/image/index/EnglishLogo.png" alt="">英文
+                                </el-dropdown-item>
+                            </el-dropdown-menu>
+                        </el-dropdown>
                     </div>
                 </div>
+                <el-dialog title="个人设置" width="500px" :visible.sync="headerNav.dialogFormVisible">
+                    <el-tabs type="border-card">
+                        <el-tab-pane label="头像设置">
+                            <div class="uploadHead">
+                                <img src="./assets/1.jpg" alt="">
+                                <p><el-button type="primary">上传头像</el-button></p>
+                            </div>
+                            <el-row :gutter="20">
+                                <el-col :span="4"><img class="userImg" src="./assets/1.jpg" alt=""></el-col>
+                                <el-col :span="4"><img class="userImg" src="./assets/1.jpg" alt=""></el-col>
+                                <el-col :span="4"><img class="userImg" src="./assets/1.jpg" alt=""></el-col>
+                                <el-col :span="4"><img class="userImg" src="./assets/1.jpg" alt=""></el-col>
+                                <el-col :span="4"><img class="userImg" src="./assets/1.jpg" alt=""></el-col>
+                                <el-col :span="4"><img class="userImg" src="./assets/1.jpg" alt=""></el-col>
+                            </el-row>
+                        </el-tab-pane>
+                        <el-tab-pane label="个人资料">
+                            <el-form label-width="80px" :model="headerNav.formUser">
+                                <el-form-item label="登录名">
+                                    <el-input v-model="headerNav.formUser.loginName"></el-input>
+                                </el-form-item>
+                                <el-form-item label="用户名">
+                                    <el-input v-model="headerNav.formUser.name"></el-input>
+                                </el-form-item>
+                                <el-form-item label="用户状态">
+                                    <el-input v-model="headerNav.formUser.name"></el-input>
+                                </el-form-item>
+                                <el-form-item label="邮箱">
+                                    <el-input v-model="headerNav.formUser.email"></el-input>
+                                </el-form-item>
+                                <el-form-item label="电话">
+                                    <el-input v-model="headerNav.formUser.tel"></el-input>
+                                </el-form-item>
+                            </el-form>
+                        </el-tab-pane>
+                        <el-tab-pane label="密码设置">角色管理</el-tab-pane>
+                    </el-tabs>
+                    <div slot="footer" class="dialog-footer">
+                        <el-button @click="headerNav.dialogFormVisible = false">取 消</el-button>
+                        <el-button type="primary" @click="headerNav.dialogFormVisible = false">确 定</el-button>
+                    </div>
+                </el-dialog>
             </div>
         </div>
     </div>
@@ -62,7 +135,9 @@
 
 <script>
     import $ from "jquery";
-    import SideNavList from "./components/index/sideNav-list"
+    import SideNavList from "./components/index/sideNav-list";
+    import ChinaLogo from "../public/image/index/ChinaLogo.png"
+    import EnglishLogo from "../public/image/index/EnglishLogo.png"
 
     export default {
         name: "app",
@@ -75,7 +150,7 @@
                         activeColor: "#ff7b62",
                         headerNav: "#11a0f8",
                         hoverColor: "",
-                        hoverBackgroundColor:""
+                        hoverBackgroundColor: ""
                     },
                     data: []
                 },
@@ -84,21 +159,60 @@
                     icon: "el-icon-s-fold",
                     sideNavOn: false,
                     imgOne: false,
+                    language: {
+                        img: ChinaLogo,
+                        text: "中文"
+                    },
+                    dialogFormVisible: false,
+                    formUser:{
+                        loginName: "",
+                        name:"",
+                        state: "",
+                        email: "",
+                        tel: ""
+                    }
                 }
 
             }
         },
-        components:{
+        components: {
             SideNavList
         },
         methods: {
             //点击收起展开菜单栏
             navIconOn() {
-                this.headerNav.sideNavOn = this.headerNav.sideNavOn ? false : true;
+                this.headerNav.sideNavOn = !this.headerNav.sideNavOn;
+            },
+            //个人设置
+            userDrop(command) {
+                const that = this;
+                switch (Number(command)) {
+                    case 1:
+                        that.headerNav.dialogFormVisible = true;
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    default:
+
+                }
+            },
+            //语言选择
+            languageDrop(command) {
+                if (command == "china") {
+                    this.headerNav.language = {img: ChinaLogo, text: "中文"}
+                } else {
+                    this.headerNav.language = {img: EnglishLogo, text: "英文"}
+                }
             },
             //头部展开动画
             navImgone(e) {
-                this.headerNav.imgOne = this.headerNav.imgOne ? false : true;
+                this.headerNav.imgOne = !this.headerNav.imgOne;
                 let width = $(e.target).prev().children("li").length * 90;
                 if (this.headerNav.imgOne) {
                     $(e.target).prev().animate({
@@ -131,24 +245,24 @@
                 }
             },
         },
-        mounted(){
+        mounted() {
             //侧边导航数据
-            this.$post("/backstage/menu/getMenuJson3",{ref:"n"},{dataType:"json"}).then((data)=>{
+            /*this.$post("/backstage/menu/getMenuJson3", {ref: "n"}, {dataType: "json"}).then((data) => {
                 this.sideNav.data = data.obj.menuList;
-            });
+            });*/
             //侧边导航颜色 /backstage/account/getTheme
-            this.$post("/backstage/account/getTheme").then((data)=>{
+            this.$post("/backstage/account/getTheme").then((data) => {
                 let val = JSON.parse(data.obj);
-                this.$set(this.sideNav.color,"headerNav",val[0]);
-                this.$set(this.sideNav.color,"backgroundColor",val[1]);
-                this.$set(this.sideNav.color,"textColor",val[2]);
-                this.$set(this.sideNav.color,"hoverColor",val[3]);
-                this.$set(this.sideNav.color,"hoverBackgroundColor",val[4]);
-                this.$set(this.sideNav.color,"activeColor",val[5]);
+                this.$set(this.sideNav.color, "headerNav", val[0]);
+                this.$set(this.sideNav.color, "backgroundColor", val[1]);
+                this.$set(this.sideNav.color, "textColor", val[2]);
+                this.$set(this.sideNav.color, "hoverColor", val[3]);
+                this.$set(this.sideNav.color, "hoverBackgroundColor", val[4]);
+                this.$set(this.sideNav.color, "activeColor", val[5]);
 
                 let style = document.createElement("style");
                 style.type = "text/css";
-                this.$store.commit("addStyle",style);
+                this.$store.commit("addStyle", style);
                 style.innerHTML = `
                 #sideNav .el-submenu__title:hover{
                     background-color: ${val[4]} !important;
@@ -165,8 +279,8 @@
                 document.head.appendChild(style);
             });
         },
-        beforeDestroy(){
-            if(this.$store.getters.getStyle){
+        beforeDestroy() {
+            if (this.$store.getters.getStyle) {
                 document.head.removeChild(this.$store.getters.getStyle);
             }
         }
@@ -327,6 +441,13 @@
                 font-size: 21px;
                 cursor: pointer;
             }
+
+            .el-badge__content.is-fixed {
+                top: 14px;
+                height: 14px;
+                line-height: 14px;
+                padding: 0 3px;
+            }
             .nav-language {
                 font-size: 14px;
                 img {
@@ -336,6 +457,31 @@
                 span {
                     cursor: pointer;
                 }
+            }
+        }
+
+        .el-dialog__body{
+            height: 300px;
+            padding-top: 12px;
+
+            .uploadHead{
+                text-align: center;
+                img{
+                    width: 100px;
+                    height: 100px;
+                    border-right: 5px;
+                }
+                p{
+                    margin-top: 10px;
+                }
+            }
+            .el-row{
+                margin-top: 40px;
+            }
+            .userImg{
+                width: 50px;
+                height: 50px;
+                border-radius: 5px;
             }
         }
     }
